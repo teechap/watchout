9f462b1b0d59a00df0f4f86f3e8b6528a506e81d
teechap <travisechapman@gmail.com>
2014-12-19 20:13:27 -0800

    Almost done

--- coolGame.js
+++ coolGame.js
@@ -12,10 +12,24 @@ var data = [
   {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
   {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"}
 ];
+var player = [
+  {"width": 12, "height": 12, "color": "green"},
+  ]
+
+
+
+
 
 var svgContainer = d3.select('body')
 .select('.playground')
-.append('svg');
+.append('svg').on("mousemove", function(){
+
+  var pt = d3.mouse(this);
+  console.log(pt);
+  //reset squares x and y coordinates
+
+
+})  //on mouse enter svg put the player element where the mouse is
 
 //create initialize function
 var circles = svgContainer.selectAll('circle')
@@ -24,36 +38,77 @@ var circles = svgContainer.selectAll('circle')
 .append('circle')
 
 //call initialize
+var playerContainer = d3.select('body')
+.select('.playground')
+.select('svg')  //on mouse enter svg put the player element where the mouse is
+
+var playerCircles = playerContainer.selectAll('rect')
+.data(player)
+.enter()
+.append('rect')
+
+var playerAttrs = playerCircles
+  .style("fill", function(d){return d.color;})
+.attr("width", 12)
+.attr("height", 12)
 
 
-function update(data){
-  console.log("in update!")
 
 
-  console.log('DATA: ', data);
+
+function enemyUpdate(data){
+
+
+
   var circleAttrs = circles
   .transition().duration(1500)
   .attr("cx", function(d){return makeRandomX(d.radius);})
   .attr("cy", function(d){return makeRandomY(d.radius);})
   .attr("r", function(d){return d.radius;})
-  .style("fill", function(d){return d.color;});
+  .style("fill", function(d){return d.color;})
 
 };
 
 var makeRandomX = function(radius){
-  var newX = Math.floor(Math.random()*(900-radius));
+  var newX = Math.floor(Math.random()*(960-radius));
+  if(newX < radius){
+    newX = radius;
+  }
   return newX;
 }
 
 var makeRandomY = function(radius){
   var newY = Math.floor(Math.random()*(500-radius));
+  if(newY < radius){
+    newY = radius;
+  }
   return newY;
 }
 
 setInterval(function(){
 
-  update(data)
+  enemyUpdate(data)
 },2000);
 
-//set an interval
-//
+//set an interval for player
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
