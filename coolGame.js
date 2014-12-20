af5ccc28585f157aa5e7275ad8eac81127ad116e
teechap <travisechapman@gmail.com>
2014-12-20 12:32:08 -0800

    trying to get collision to work

--- coolGame.js
+++ coolGame.js
@@ -1,19 +1,19 @@
 
 var data = [
-  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  {"x_axis": 480, "y_axis": 250, "radius": 20, "color": "#00326D"},
-  {"x_axis": 480, "y_axis": 250, "radius": 100, "color": "#00326D"},
-  {"x_axis": 480, "y_axis": 250, "radius": 50, "color": "#00326D"},
-  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
   {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"}
+  // {"x_axis": 480, "y_axis": 250, "radius": 20, "color": "#00326D"},
+  // {"x_axis": 480, "y_axis": 250, "radius": 100, "color": "#00326D"},
+  // {"x_axis": 480, "y_axis": 250, "radius": 50, "color": "#00326D"},
+  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"}
 ];
 var player = [
-  {"width": 12, "height": 12, "color": "green"},
+  {"x": 30, "y": 30, "width": 12, "height": 12, "color": "green"},
   ]
 
 
@@ -22,50 +22,110 @@ var player = [
 
 var svgContainer = d3.select('body')
 .select('.playground')
-.append('svg').on("mousemove", function(){
+.append('svg').on("mousemove", function(d){
+  var position = d3.mouse(this);
+  d3.selectAll(".player")
+  .attr('x', position[0]-6)
+  .attr('y', position[1]-6)
 
-  var pt = d3.mouse(this);
-  console.log(pt);
-  //reset squares x and y coordinates
+})
 
 
-})  //on mouse enter svg put the player element where the mouse is
-
-//create initialize function
 var circles = svgContainer.selectAll('circle')
 .data(data)
 .enter()
-.append('circle')
+.append('circle').on("mouseenter", function(){
+  d3.selectAll(".player")
+    // console.log("HIT")
+  // .transition()
+  // .duration(1)
+  // .style("fill", "red")  //toggle on and off red fill to show user there was a collision
+  // .transition()
+  // .duration(100)
+  // .style("fill", "green")
+})
 
-//call initialize
+
 var playerContainer = d3.select('body')
 .select('.playground')
-.select('svg')  //on mouse enter svg put the player element where the mouse is
+.select('svg')
 
 var playerCircles = playerContainer.selectAll('rect')
 .data(player)
 .enter()
 .append('rect')
 
+
 var playerAttrs = playerCircles
   .style("fill", function(d){return d.color;})
 .attr("width", 12)
 .attr("height", 12)
+.attr("x", 30)
+.attr("y", 30)
+.classed("player", true)
+
+// var playerTween = function(){
+//   var rectx = d3.select(this);
+//   return function(){
+//     console.log(rectx.attr("x"));
+//   };
+// }
+
+var myTween = function(){
+  // console.log(d3.select(this));
+  var obj = d3.select(this);
+
+
+  return function(){
+    var px = d3.select('body')
+    .select('.playground')
+    .select('svg')
+    .selectAll('rect')
+    .attr("x")
+
+     var py = d3.select('body')
+    .select('.playground')
+    .select('svg')
+    .selectAll('rect')
+    .attr("y")
+
+    var pWidth = d3.select('body')
+      .select('.playground')
+      .select('svg')
+      .selectAll('rect')
+      .attr("width")
+
+    var enemyX = obj.attr("cx")
+    var enemyY = obj.attr("cy")
+    var enemyR = obj.attr("r")
 
 
 
+    if( Math.abs(enemyX - px) < 10  && Math.abs(enemyY - py) < 10 ){
+      console.log("HIT")
+    }
+
+
+
+
+  }
+}
 
 
 function enemyUpdate(data){
 
-
+  debugger;
 
   var circleAttrs = circles
   .transition().duration(1500)
+  .tween("myTween", myTween)
   .attr("cx", function(d){return makeRandomX(d.radius);})
   .attr("cy", function(d){return makeRandomY(d.radius);})
   .attr("r", function(d){return d.radius;})
   .style("fill", function(d){return d.color;})
+
+
+
 
 };
 
@@ -91,10 +151,6 @@ setInterval(function(){
 },2000);
 
 //set an interval for player
-
-
-
-
 
 
 
