a010f541837342215ecb8581060fa10b44c01ccb
teechap <travisechapman@gmail.com>
2014-12-20 16:03:53 -0800

    scoreBoard working

--- coolGame.js
+++ coolGame.js
@@ -1,24 +1,67 @@
 
 var data = [
+  {"x_axis": 480, "y_axis": 250, "radius": 20, "color": "#00326D"},
+  {"x_axis": 480, "y_axis": 250, "radius": 20, "color": "#00326D"},
+  {"x_axis": 480, "y_axis": 250, "radius": 100, "color": "#00326D"},
+  {"x_axis": 480, "y_axis": 250, "radius": 50, "color": "#00326D"},
+  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
+  {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
   {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"}
-  // {"x_axis": 480, "y_axis": 250, "radius": 20, "color": "#00326D"},
-  // {"x_axis": 480, "y_axis": 250, "radius": 100, "color": "#00326D"},
-  // {"x_axis": 480, "y_axis": 250, "radius": 50, "color": "#00326D"},
-  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"},
-  // {"x_axis": 480, "y_axis": 250, "radius": 10, "color": "#00326D"}
 ];
 var player = [
   {"x": 30, "y": 30, "width": 12, "height": 12, "color": "green"},
   ]
 
 
+var highScore = 0;
+var currentScore = 0;
 
 
+
+var scoreInc = function(){
+  currentScore++;
+  updateScore();
+  // updateScoreboardText();
+}
+setInterval(scoreInc, 200);
+
+var scoreboard = function(){
+  if(currentScore > highScore){
+    highScore = currentScore;
+  }
+  currentScore = 0;
+  updateScore();
+}
+
+
+var updateScore = function(){
+  d3.select('.scoreboard #high span').text(highScore);
+  d3.select('.scoreboard #current span').text(currentScore);
+}
+
+
+
+// var updateScoreboardText = function(){
+//   var high = document.getElementById("high");
+//   var current = document.getElementById("current");
+//   var collisions = document.getElementById("collisions");
+//   console.log(highScore, currentScore);
+//   high.innerHTML = highScore.toString();
+//   current.innerHTML = currentScore.toString();
+// }
+
+// var blah = function(){
+//   var scoreBoard = d3.select('body')
+//   .select('.scoreboard')
+//   .data([currentScore, highScore, collisions])
+//   .selectAll('div')
+//   .transition()
+//   .text(currentScore);
+// }
 
 var svgContainer = d3.select('body')
 .select('.playground')
@@ -60,8 +103,10 @@ var playerAttrs = playerCircles
   .style("fill", function(d){return d.color;})
 .attr("width", 12)
 .attr("height", 12)
-.attr("x", 30)
-.attr("y", 30)
+// .attr("x", 30)
+// .attr("y", 30)
+.attr("rx", 20)
+.attr("ry", 20)
 .classed("player", true)
 
 // var playerTween = function(){
@@ -70,7 +115,7 @@ var playerAttrs = playerCircles
 //     console.log(rectx.attr("x"));
 //   };
 // }
-
+var count=0;
 var myTween = function(){
   // console.log(d3.select(this));
   var obj = d3.select(this);
@@ -89,20 +134,23 @@ var myTween = function(){
     .selectAll('rect')
     .attr("y")
 
-    var pWidth = d3.select('body')
+    var rx = d3.select('body')
       .select('.playground')
       .select('svg')
       .selectAll('rect')
-      .attr("width")
+      .attr("rx")
 
     var enemyX = obj.attr("cx")
     var enemyY = obj.attr("cy")
     var enemyR = obj.attr("r")
 
+    var rx = Number(rx);
+    var enemyR = Number(enemyR);
+    if( (Math.abs(enemyX - px)) < (rx+enemyR)  && (Math.abs(enemyY - py)) < (rx+enemyR)){
+      //call score function
+      scoreboard();
+      console.log("HIT: ", count)
 
-
-    if( Math.abs(enemyX - px) < 10  && Math.abs(enemyY - py) < 10 ){
-      console.log("HIT")
     }
 
 
@@ -114,7 +162,7 @@ var myTween = function(){
 
 function enemyUpdate(data){
 
-  debugger;
+
 
   var circleAttrs = circles
   .transition().duration(1500)
@@ -152,6 +200,9 @@ setInterval(function(){
 
 //set an interval for player
 
+// setInterval(function(){
+//   blah();
+// }, 200);
 
 
 
